a6cba3ca180540d5343de2adeb09c93d6a9f43c8
AbdelaliAAZZ <abdelaliaazzaoui@gmail.com>
2024-12-22 04:20:36 +0100

    update 1

--- src/FlashCards.jsx
+++ src/FlashCards.jsx
@@ -4,14 +4,15 @@ import { useState } from "react";
 
 function FlashCards({questions}) {
 
-    const [hovered, setHovered] = useState(false);
+    const [clicked, setClicked] = useState(false);
+     
      
 
     console.log(questions)
   return (
   
-    <div onClick={() => setHovered(!hovered)}  className={hovered ? "selected" : "flashcard"}>
-        {hovered ? questions.answer : questions.question}
+    <div onClick={() => setClicked(!clicked)}  className={clicked ? "selected" : "flashcard"}>
+        {clicked ? questions.answer : questions.question}
     </div>
   )
 }
